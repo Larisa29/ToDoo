5c1545f69bfbf15f7407b30da4a592288402715d
Talmacel Larisa <larisa-maria.talmacel@student.tuiasi.ro>
2024-11-19 10:37:57 +0200

    Added PUT endpoint for updating tasks

--- tasks-app-server/src/index.ts
+++ tasks-app-server/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import { PrismaClient } from "@prisma/client";
 
@@ -9,23 +9,72 @@ app.use(express.json())
 app.use(cors())
 
 
-app.get("/api/tasks", async (req, res) => {
+app.get("/api/tasks", async (req: Request, res: Response) => {
     const tasks = await prisma.task.findMany();
 
     res.json(tasks);
 });
 
-app.post("/api/tasks", async (req, res) => {
-    const {title, content} = req.body;
+app.post("/api/tasks", async (req: Request, res: Response) => {
+    const { title, content } = req.body;
 
-    const task = await prisma.task.create({
-        data: {title, content},
-    });
+    if (!title || !content) {
+        //res.status(400).json({ error: "Title and content fields required." });
+        res.status(400).send("Title and content fields required.");
+        return;
+    }
 
-    res.status(201).json(task);
+    try {
+        const task = await prisma.task.create({
+            data: { title, content },
+
+        });
+
+        res.status(201).json(task);
+    }
+    catch (error) {
+        res.status(500).send("oops I did it again..");
+    }
 });
 
-app.listen(5000, ()=>{
+app.put("/api/tasks/:id", async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
+    if (!id || isNaN(id)) {
+        res.status(400).send("Id must be a valid number!!");
+        return;
+    }
+
+    const existingTask = await prisma.task.findUnique({
+        where:{
+            id : id
+        }
+    })
+
+    if (!existingTask){
+        res.status(404).send(`Task with id ${id} not found.`);
+        return;
+    }
+    
+    const { title, content } = req.body;
+
+    try {
+        const updatedTask = await prisma.task.update({
+            where: {
+                id: id
+            },
+            data: {
+                title: title,
+                content: content
+            }
+        });
+
+        res.status(201).json(updatedTask);
+    } catch (error) {
+        res.status(500).send("ooops i did it again..");
+    }
+})
+
+app.listen(5000, () => {
     console.log("Server running on localhost:5000")
 })
 
